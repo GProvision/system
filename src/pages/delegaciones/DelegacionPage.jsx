a6ac8ecb59912f7554038a6d52389975ed810da0
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-10-06 14:10:15 -0300

    Add Dotenv and Delegaciones

--- src/pages/delegaciones/DelegacionPage.jsx
+++ src/pages/delegaciones/DelegacionPage.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
-import { X, Save, Pen, Info, ChevronLeft, Trash2 } from "lucide-react";
+import { X, Save, Pen, Info, ChevronLeft, Trash2, Loader } from "lucide-react";
 import { toast } from "react-hot-toast";
 import Swal from "sweetalert2";
 
@@ -9,6 +9,10 @@ const DelegacionPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const [delegacion, setDelegacion] = useState(null);
+  const [provincia, setProvincia] = useState(null);
+  const [localidad, setLocalidad] = useState(null);
+  const [provincias, setProvincias] = useState([]);
+  const [localidades, setLocalidades] = useState([]);
   const [isEditFormOpen, setIsEditFormOpen] = useState(false);
   const {
     register,
@@ -18,7 +22,8 @@ const DelegacionPage = () => {
     formState: { errors, isSubmitting },
   } = useForm({
     defaultValues: {
-      nombre: "",
+      provincia: "",
+      localidad: "",
       activo: false,
     },
   });
@@ -26,20 +31,109 @@ const DelegacionPage = () => {
     const response = await fetch(`/api/delegaciones/${id}`);
     const data = await response.json();
     setDelegacion(data);
-    setValue("nombre", data.nombre);
+    setValue("provincia", data.provincia);
+    setValue("localidad", data.localidad);
     setValue("activo", data.activo);
   };
+  const getProvincias = async () => {
+    const response = await fetch(
+      "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre&max=100"
+    );
+    const data = await response.json();
+    const sortedProvincias = data.provincias.sort((a, b) =>
+      a.nombre.localeCompare(b.nombre)
+    );
+    setProvincias(sortedProvincias);
+    if (delegacion?.provincia) {
+      const provinciaEncontrada = sortedProvincias.find(
+        (p) => p.nombre === delegacion.provincia
+      );
+      if (provinciaEncontrada) {
+        setProvincia(provinciaEncontrada.id);
+      }
+    }
+  };
+  const getLocalidades = async () => {
+    if (!provincia) return;
+    const response = await fetch(
+      `https://apis.datos.gob.ar/georef/api/localidades?provincia=${provincia}&campos=id,nombre&max=1000`
+    );
+    const data = await response.json();
+    const sortedLocalidades = data.localidades.sort((a, b) =>
+      a.nombre.localeCompare(b.nombre)
+    );
+    setLocalidades(sortedLocalidades);
+    if (delegacion?.localidad) {
+      const localidadEncontrada = sortedLocalidades.find(
+        (l) => l.nombre === delegacion.localidad
+      );
+      if (localidadEncontrada) {
+        setLocalidad(localidadEncontrada.id);
+      }
+    }
+  };
+
+  const editDelegacion = async (data) => {
+    const { provincia, localidad, activo } = data;
+
+    if (!provincia) {
+      toast.error("Debe seleccionar una provincia");
+      return;
+    }
+    if (!localidad) {
+      toast.error("Debe seleccionar una localidad");
+      return;
+    }
+    try {
+      const response = await fetch("/api/delegaciones/update", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: delegacion.id,
+          provincia,
+          localidad,
+          activo: Boolean(activo),
+        }),
+      });
+      if (response.ok) {
+        await getDelegacion();
+        setIsEditFormOpen(false);
+        toast.success("Delegacion editada exitosamente");
+        reset();
+      } else {
+        const { error } = await response.json();
+        throw new Error(error);
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
   useEffect(() => {
     getDelegacion();
-  }, []);
-  if (!delegacion) return null;
+  }, [id]);
+
+  useEffect(() => {
+    if (delegacion) {
+      getProvincias();
+    }
+  }, [delegacion]);
+
+  useEffect(() => {
+    if (provincia) {
+      getLocalidades();
+    }
+  }, [provincia]);
+  if (!delegacion || !provincias || !localidades) return null;
   return (
     <section className="container mx-auto px-4 py-8 max-w-6xl">
       <header className="flex justify-between items-center mb-4">
         <div className="flex items-center gap-2">
           <button
             type="button"
-            onClick={() => navigate("/sindicatos")}
+            onClick={() => navigate("/delegaciones")}
             className=" cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors"
             aria-label="Cancelar edición del sindicato"
           >
@@ -62,7 +156,7 @@ const DelegacionPage = () => {
           <button
             type="button"
             onClick={() => setIsEditFormOpen(true)}
-            className=" cursor-pointer inline-flex items-center justify-center p-4 text-sm font-medium text-gray-900 bg-yellow-100 border border-yellow-300 rounded-md hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition-colors"
+            className=" cursor-pointer inline-flex items-center justify-center p-2 text-sm font-medium text-gray-900 bg-yellow-100 border border-yellow-300 rounded-md hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 transition-colors"
             aria-label="Editar Delegación"
           >
             <Pen className="size-4" />
@@ -70,13 +164,140 @@ const DelegacionPage = () => {
           <button
             type="button"
             onClick={() => console.log("Eliminar")}
-            className=" cursor-pointer inline-flex items-center justify-center p-4 text-sm font-medium text-gray-700 bg-red-100 border border-red-300 rounded-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
+            className=" cursor-pointer inline-flex items-center justify-center p-2 text-sm font-medium text-gray-700 bg-red-100 border border-red-300 rounded-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
             aria-label="Eliminar Delegación"
           >
             <Trash2 className="size-4" />
           </button>
         </div>
       </header>
+      {isEditFormOpen && (
+        <section className="mb-8 p-6 bg-white rounded-lg shadow-sm border border-gray-200">
+          <header className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-bold text-gray-900">
+              Editar Delegacion
+            </h2>
+            <button
+              className="inline-flex items-center justify-center p-2 text-sm font-medium text-gray-700 bg-red-100 border border-red-300 rounded-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
+              aria-label="Cancelar edición del sindicato"
+              onClick={() => setIsEditFormOpen(false)}
+            >
+              <X className="size-4 text-red-500" />
+            </button>
+          </header>
+          <form
+            className="flex flex-col gap-4 items-center justify-start max-w-xl mx-auto"
+            onSubmit={handleSubmit(editDelegacion)}
+          >
+            <fieldset className="w-full flex flex-col items-center justify-start">
+              <label
+                className="block text-sm font-medium text-gray-700 mb-2"
+                htmlFor="provincia"
+              >
+                Provincia
+                <span className="text-red-500" aria-label="campo requerido">
+                  *
+                </span>
+              </label>
+              <select
+                defaultValue={provincia}
+                aria-required="true"
+                aria-describedby="provincia-help"
+                className={`block cursor-pointer w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-sm ${
+                  errors.provincia
+                    ? "border-red-300 bg-red-50"
+                    : "border-gray-300 hover:border-gray-400"
+                }`}
+                id="provincia"
+                onChange={(e) => {
+                  setProvincia(e.target.value);
+                  setValue("localidad", "");
+                  setLocalidad(null);
+                  setValue(
+                    "provincia",
+                    provincias.find((p) => p.id === e.target.value)?.nombre ||
+                      ""
+                  );
+                }}
+              >
+                <option value="">Seleccione una provincia</option>
+                {provincias.map((provincia) => (
+                  <option key={provincia.id} value={provincia.id}>
+                    {provincia.nombre}
+                  </option>
+                ))}
+              </select>
+            </fieldset>
+            <fieldset className="w-full flex flex-col items-center justify-start">
+              <label
+                className="block text-sm font-medium text-gray-700 mb-2"
+                htmlFor="localidad"
+              >
+                Localidad
+                <span className="text-red-500" aria-label="campo requerido">
+                  *
+                </span>
+              </label>
+              <select
+                defaultValue={localidad || ""}
+                aria-required="true"
+                aria-describedby="localidad-help"
+                className={`block cursor-pointer w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-sm ${
+                  errors.localidad
+                    ? "border-red-300 bg-red-50"
+                    : "border-gray-300 hover:border-gray-400"
+                }`}
+                id="localidad"
+                disabled={!provincia}
+                onChange={(e) => {
+                  setLocalidad(e.target.value);
+                  setValue(
+                    "localidad",
+                    localidades.find((l) => l.id === e.target.value)?.nombre ||
+                      ""
+                  );
+                }}
+              >
+                <option value="">Seleccione una localidad</option>
+                {localidades.map((localidad) => (
+                  <option key={localidad.id} value={localidad.id}>
+                    {localidad.nombre}
+                  </option>
+                ))}
+              </select>
+            </fieldset>
+            <fieldset className="flex items-center gap-1">
+              <input
+                id="activo"
+                type="checkbox"
+                aria-describedby="activo-help"
+                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded transition-colors"
+                {...register("activo")}
+              />
+              <label
+                htmlFor="activo"
+                className="text-sm font-medium text-gray-700 cursor-pointer"
+              >
+                Activo
+              </label>
+              <div id="activo-help" className="sr-only">
+                Indica si el sindicato está activo
+              </div>
+            </fieldset>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white flex cursor-pointer items-center justify-center p-2 rounded"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <Loader className="size-4 animate-spin" />
+              ) : (
+                <Save className="size-4" />
+              )}
+            </button>
+          </form>
+        </section>
+      )}
     </section>
   );
 };
--- src/pages/delegaciones/DelegacionesPage.jsx
+++ src/pages/delegaciones/DelegacionesPage.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
 import { toast } from "react-hot-toast";
-import { Plus } from "lucide-react";
+import { Loader, Plus, Save, X } from "lucide-react";
 
 const DelegacionesPage = () => {
   const [delegaciones, setDelegaciones] = useState([]);
@@ -11,6 +11,7 @@ const DelegacionesPage = () => {
   const [provinciaId, setProvinciaId] = useState(null);
   const [isAddFormOpen, setIsAddFormOpen] = useState(false);
   const navigate = useNavigate();
+  const [page, setPage] = useState(1);
   const {
     handleSubmit,
     setValue,
@@ -99,15 +100,45 @@ const DelegacionesPage = () => {
           onClick={() => setIsAddFormOpen(true)}
           className="bg-blue-500 hover:bg-blue-600 text-white p-2 flex items-center justify-center rounded"
         >
-          <Plus className="size-6" />
+          <Plus className="size-4" />
         </button>
       </header>
       {isAddFormOpen && (
-        <section>
-          <form onSubmit={handleSubmit(addDelegacion)}>
-            <fieldset>
-              <label htmlFor="provincia">Provincia</label>
+        <section className="mb-8 p-6 bg-white rounded-lg shadow-sm border border-gray-200">
+          <header className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-bold text-gray-900">
+              Agregar Delegacion
+            </h2>
+            <button
+              className="inline-flex items-center justify-center p-2 text-sm font-medium text-gray-700 bg-red-100 border border-red-300 rounded-md hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors"
+              aria-label="Cancelar edición del sindicato"
+              onClick={() => setIsAddFormOpen(false)}
+            >
+              <X className="size-4 text-red-500" />
+            </button>
+          </header>
+          <form
+            className="flex flex-col gap-4 items-center justify-start max-w-xl mx-auto"
+            onSubmit={handleSubmit(addDelegacion)}
+          >
+            <fieldset className="w-full flex flex-col items-center justify-start">
+              <label
+                className="block text-sm font-medium text-gray-700 mb-2"
+                htmlFor="provincia"
+              >
+                Provincia
+                <span className="text-red-500" aria-label="campo requerido">
+                  *
+                </span>
+              </label>
               <select
+                aria-required="true"
+                aria-describedby="provincia-help"
+                className={`block cursor-pointer w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-sm ${
+                  errors.provincia
+                    ? "border-red-300 bg-red-50"
+                    : "border-gray-300 hover:border-gray-400"
+                }`}
                 id="provincia"
                 onChange={(e) => {
                   setProvinciaId(e.target.value);
@@ -126,9 +157,24 @@ const DelegacionesPage = () => {
                 ))}
               </select>
             </fieldset>
-            <fieldset>
-              <label htmlFor="localidad">Localidad</label>
+            <fieldset className="w-full flex flex-col items-center justify-start">
+              <label
+                className="block text-sm font-medium text-gray-700 mb-2"
+                htmlFor="localidad"
+              >
+                Localidad
+                <span className="text-red-500" aria-label="campo requerido">
+                  *
+                </span>
+              </label>
               <select
+                aria-required="true"
+                aria-describedby="localidad-help"
+                className={`block cursor-pointer w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-sm ${
+                  errors.localidad
+                    ? "border-red-300 bg-red-50"
+                    : "border-gray-300 hover:border-gray-400"
+                }`}
                 id="localidad"
                 disabled={!provinciaId}
                 onChange={(e) => {
@@ -146,7 +192,17 @@ const DelegacionesPage = () => {
                 ))}
               </select>
             </fieldset>
-            <button type="submit">Agregar Delegacion</button>
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white flex cursor-pointer items-center justify-center p-2 rounded"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <Loader className="size-4 animate-spin" />
+              ) : (
+                <Save className="size-4" />
+              )}
+            </button>
           </form>
         </section>
       )}
@@ -156,34 +212,62 @@ const DelegacionesPage = () => {
         </div>
       )}
       {delegaciones.length > 0 && (
-        <table className="min-w-full divide-y divide-gray-200 overflow-x-scroll">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Provincia
-              </th>
-              <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Localidad
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {delegaciones.map((delegacion) => (
-              <tr
-                key={delegacion.id}
-                className="hover:bg-gray-50 cursor-pointer"
-                onClick={() => navigate(`/delegaciones/${delegacion.id}`)}
-              >
-                <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium text-gray-900 ">
-                  {delegacion.provincia}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium text-gray-900 ">
-                  {delegacion.localidad}
-                </td>
+        <>
+          <table className="min-w-full divide-y divide-gray-200 overflow-x-scroll">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Provincia
+                </th>
+                <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Localidad
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {delegaciones
+                .slice((page - 1) * 10, page * 10)
+                .map((delegacion) => (
+                  <tr
+                    key={delegacion.id}
+                    className="hover:bg-gray-50 cursor-pointer"
+                    onClick={() => navigate(`/delegaciones/${delegacion.id}`)}
+                  >
+                    <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium text-gray-900 ">
+                      {delegacion.provincia}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap  text-sm font-medium text-gray-900 ">
+                      {delegacion.localidad}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+          <form className="flex justify-center items-center gap-2">
+            <button
+              type="button"
+              onClick={() => setPage(page - 1)}
+              disabled={page === 1}
+              className="cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Anterior
+            </button>
+            <output>{page}</output>
+            <button
+              type="button"
+              onClick={() => setPage(page + 1)}
+              disabled={
+                page ===
+                Math.ceil(
+                  delegaciones.slice((page - 1) * 10, page * 10).length / 10
+                )
+              }
+              className="cursor-pointer inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+            >
+              Siguiente
+            </button>
+          </form>
+        </>
       )}
     </section>
   );
